0b8b8d8bedc787214aa8f2ad93a28716fb6afa05
Seema2882001 <160244580+Seema2882001@users.noreply.github.com>
2024-02-22 21:06:21 +0530

    initial commit

---
+++ src/components/footer/index.js
@@ -0,0 +1,10 @@
+import './footer.css';
+
+const Footer =()=>{
+    return(
+        <footer className="footer">
+            Copyright &#169; 2024 seema shaik. All rights received.
+        </footer>
+    )
+}
+export default Footer;
---
+++ src/components/home/index.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import './home.css'
+import Typed from 'typed.js';
+import bg from '../../assests/images/img.webp';
+import resume from '../../assests/resume.pdf';
+
+function Home() {
+  const el = React.useRef(null);
+
+  React.useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ['Web Devloper', 'Frontend Devloper','Dancer','Gamer'],
+      // startDelay: 160,
+      typeSpeed: 130,
+      backSpeed: 50,
+      showCursor: false,
+      // fadeOut: true,
+      // fadeOutDelay: 500,
+      loop:true
+    });
+
+    return () => {
+      // Destroy Typed instance during cleanup to stop animation
+      typed.destroy();
+    };
+  }, []);
+  return (
+    <section className='home' id='home'>
+
+<div className="left-side-content">
+          <span className="hi">Hi, It's Me</span><br/>
+          <span className="blue-name name">Seema Shaik,</span><br/>
+          <span className="hi">I'm a <span className="blue-name" ref={el}></span></span><br/>
+
+          <p className="intro-para">I'm a front-end developer who likes to craft solid and scalable <span> front-end projects </span><br/>with great user experience</p>
+          <a href={resume} download={resume}><button className="abt-btn">Download cv</button></a>
+        </div>
+        <div className="right-side-content">
+            <img src={bg} className="bg-img" alt="image"/>
+            </div>
+
+    </section>
+  )
+}
+
+export default Home
